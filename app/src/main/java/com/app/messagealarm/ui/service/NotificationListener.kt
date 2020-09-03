1d8ff1cc3ac2bb61a9344dc5fbcb0f5f7afde17a
almujahidkhan <almujahid1100@gmail.com>
2020-09-03 12:02:47 +0600

    null checked at play sound

--- app/src/main/java/com/app/messagealarm/ui/service/NotificationListener.kt
+++ app/src/main/java/com/app/messagealarm/ui/service/NotificationListener.kt
@@ -36,8 +36,10 @@ class NotificationListener : NotificationListenerService() {
         Log.e(TAG, "TITLE " + sbn!!.notification.extras["android.title"])
         Log.e(TAG, "DESC " + sbn!!.notification.extras["android.text"])
         if (packageName == "com.facebook.orca") {
-            if (sbn.notification.extras["android.title"]!! != "Chat heads active") {
-                if(!MediaUtils.isPlaying()){
+            if (sbn.notification.extras["android.title"] != null ||
+                sbn.notification.extras["android.title"]!! != "Chat heads active"
+            ) {
+                if (!MediaUtils.isPlaying()) {
                     Handler().postDelayed(
                         Runnable {
                             FloatingNotification.showFloatingNotification(this)
