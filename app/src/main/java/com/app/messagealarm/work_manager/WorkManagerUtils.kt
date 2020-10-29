8dd440b5f255634e6c834b119f2a1f4ff56b8b93
Mujahid Khan <almujahid1100@gmail.com>
2020-10-29 09:23:01 +0600

    original time for work manager instead of testing time

--- app/src/main/java/com/app/messagealarm/work_manager/WorkManagerUtils.kt
+++ app/src/main/java/com/app/messagealarm/work_manager/WorkManagerUtils.kt
@@ -2,14 +2,25 @@ package com.app.messagealarm.work_manager
 
 import android.content.Context
 import androidx.work.*
+import com.app.messagealarm.utils.Constants
+import com.app.messagealarm.utils.SharedPrefUtils
+import java.lang.NumberFormatException
 import java.util.concurrent.TimeUnit
 
 class WorkManagerUtils {
     companion object{
         fun scheduleWorks(context: Context){
+            //get mute time from preferences
+            val time = SharedPrefUtils.readString(Constants.PreferenceKeys.MUTE_TIME)
+            var duration = 10
+            duration = try {
+                time.split(" ")[0].toInt()
+            }catch (e:NumberFormatException){
+                10
+            }
             val muteDismissRequest = OneTimeWorkRequest
                 .Builder(MuteDismissWorker::class.java)
-                .setInitialDelay(5, TimeUnit.MINUTES)
+                .setInitialDelay(duration.toLong(), TimeUnit.MINUTES)
                 .build()
             WorkManager.getInstance(context).enqueueUniqueWork(System.currentTimeMillis().toString(),
                 ExistingWorkPolicy.REPLACE, muteDismissRequest)
