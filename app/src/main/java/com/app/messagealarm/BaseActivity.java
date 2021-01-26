3378ba1ad65ca09f905a25687e78b4962e463299
Mujahid Khan <almujahid1100@gmail.com>
2021-01-26 17:43:00 +0600

    inital to in_app_purchase

--- app/src/main/java/com/app/messagealarm/BaseActivity.java
+++ app/src/main/java/com/app/messagealarm/BaseActivity.java
@@ -18,28 +18,6 @@ public class BaseActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //catch global exceptions
-      /*  final Thread.UncaughtExceptionHandler oldHandler =
-                Thread.getDefaultUncaughtExceptionHandler();
-        Thread.setDefaultUncaughtExceptionHandler(
-                new Thread.UncaughtExceptionHandler() {
-                    @Override
-                    public void uncaughtException(
-                            Thread paramThread,
-                            Throwable paramThrowable
-                    ) {
-                        *//*PendingIntent service = PendingIntent.getService(
-                                getApplicationContext(),
-                                1001,
-                                new Intent(getApplicationContext(), NotificationListener.class),
-                                PendingIntent.FLAG_ONE_SHOT);
-                        AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
-                        if (alarmManager != null) {
-                            alarmManager.set(AlarmManager.ELAPSED_REALTIME_WAKEUP, 1000, service);
-                        }*//*
-                        //System.exit(2);
-                    }
-                });*/
     }
 
 }
