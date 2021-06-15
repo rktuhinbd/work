452b80d92209df4b2d6e03def180a0aca52d8a8b
Mujahid Khan <almujahid1100@gmail.com>
2021-06-15 12:06:05 +0600

    working on splash new

--- app/src/main/java/com/app/messagealarm/ui/main/add_options/AddApplicationOptionView.kt
+++ app/src/main/java/com/app/messagealarm/ui/main/add_options/AddApplicationOptionView.kt
@@ -5,8 +5,8 @@ import com.app.messagealarm.model.entity.ApplicationEntity
 interface AddApplicationOptionView{
     fun onApplicationSaveSuccess()
     fun onApplicationSaveError(message:String)
-    fun onApplicationUpdateSuccess();
-    fun onApplicationUpdateError(message: String);
+    fun onApplicationUpdateSuccess()
+    fun onApplicationUpdateError(message: String)
     fun onBitmapSaveSuccess(path:String)
     fun onBitmapSaveError()
     fun onApplicationGetSuccess(app:ApplicationEntity)
