9ca49d0e4de79eab62ef8d702acfd83b10acbd8d
Mujahid Khan <mkhan9047@gmail.com>
2022-06-23 15:59:56 +0600

    Buy pro screen more design adjustments

--- app/src/main/java/com/app/messagealarm/ui/buy_pro/ReviewList.kt
+++ app/src/main/java/com/app/messagealarm/ui/buy_pro/ReviewList.kt
@@ -7,10 +7,10 @@ import com.app.messagealarm.R
  */
 fun generateReviewList(): List<MyReviewView> {
     val reviewList = ArrayList<MyReviewView>()
-    reviewList.add(MyReviewView(R.drawable.image_brain, "Brain Baur", R.drawable.flag_america,
+    reviewList.add(MyReviewView(R.drawable.image_brain, "Brian Baur", R.drawable.flag_america,
         "Fantastic app!! Thanks to the premium version you will say goodbye to the announces forever!!"))
-    reviewList.add(MyReviewView(R.drawable.image_brain, "Brain Baur", R.drawable.flag_america,
-        "Fantastic app!! Thanks to the premium version you will say goodbye to the announces forever!!"))
+    reviewList.add(MyReviewView(R.drawable.nathan, "Nathan Kouamou", R.drawable.cameroon,
+        "Why on earth, it was not discovered earlier. Total worth it, and useful small tool!"))
     reviewList.add(MyReviewView(R.drawable.image_brain, "Brain Baur", R.drawable.flag_america,
         "Fantastic app!! Thanks to the premium version you will say goodbye to the announces forever!!"))
 
