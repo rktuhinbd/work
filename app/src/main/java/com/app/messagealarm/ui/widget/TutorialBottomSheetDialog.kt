f6b6a3f422be03806e8f22728db07bb14353c99f
Mujahid Khan <mkhan9047@gmail.com>
2022-07-05 18:00:29 +0600

    playing without internet error handled

--- app/src/main/java/com/app/messagealarm/ui/widget/TutorialBottomSheetDialog.kt
+++ app/src/main/java/com/app/messagealarm/ui/widget/TutorialBottomSheetDialog.kt
@@ -5,6 +5,7 @@ import android.app.Dialog
 import android.content.DialogInterface
 import android.os.Bundle
 import android.util.DisplayMetrics
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -12,17 +13,22 @@ import android.widget.FrameLayout
 import android.widget.TextView
 import com.app.messagealarm.R
 import com.app.messagealarm.ui.main.alarm_applications.AlarmApplicationActivity
+import com.app.messagealarm.utils.AndroidUtils
 import com.app.messagealarm.utils.Constants
 import com.app.messagealarm.utils.SharedPrefUtils
+import com.example.loadinganimation.LoadingAnimation
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.google.android.material.button.MaterialButton
 import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.PlayerConstants
 import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.YouTubePlayer
 import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.YouTubePlayerListener
 import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.options.IFramePlayerOptions
 import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.views.YouTubePlayerView
+import es.dmoral.toasty.Toasty
 import kotlinx.android.synthetic.main.layout_dialog_onboarding.*
+import pl.droidsonroids.gif.GifImageView
 
 
 class TutorialBottomSheetDialog(val activity: Activity) : BottomSheetDialogFragment() {
@@ -59,6 +65,82 @@ class TutorialBottomSheetDialog(val activity: Activity) : BottomSheetDialogFragm
     }
 
 
+    private fun showYoutubePlayer(v:View){
+        val youtubeView = v.findViewById<YouTubePlayerView>(R.id.youtube_player_view)
+        val gifView = v.findViewById<GifImageView>(R.id.gif_no_internet)
+        val retryButton = v.findViewById<MaterialButton>(R.id.button_retry)
+        val loadingAnimation = v.findViewById<LoadingAnimation>(R.id.progress_bar_video_loading)
+        loadingAnimation.visibility = View.VISIBLE
+
+        gifView?.visibility = View.GONE
+        retryButton?.visibility = View.GONE
+        youtubeView?.visibility = View.VISIBLE
+        youtubeView?.enterFullScreen()
+
+
+        youtubeView?.addYouTubePlayerListener(object : YouTubePlayerListener{
+            override fun onApiChange(youTubePlayer: YouTubePlayer) {
+
+            }
+
+            override fun onCurrentSecond(youTubePlayer: YouTubePlayer, second: Float) {
+
+            }
+
+            override fun onError(youTubePlayer: YouTubePlayer, error: PlayerConstants.PlayerError) {
+                requireActivity().runOnUiThread {
+                    Toasty.error(requireActivity(), error.name).show()
+                }
+            }
+
+            override fun onPlaybackQualityChange(
+                youTubePlayer: YouTubePlayer,
+                playbackQuality: PlayerConstants.PlaybackQuality
+            ) {
+
+            }
+
+            override fun onPlaybackRateChange(
+                youTubePlayer: YouTubePlayer,
+                playbackRate: PlayerConstants.PlaybackRate
+            ) {
+
+            }
+
+            override fun onReady(youTubePlayer: YouTubePlayer) {
+
+            }
+
+            override fun onStateChange(
+                youTubePlayer: YouTubePlayer,
+                state: PlayerConstants.PlayerState
+            ) {
+                    if(state == PlayerConstants.PlayerState.PLAYING ||
+                            state == PlayerConstants.PlayerState.UNSTARTED){
+                        v.findViewById<LoadingAnimation>(R.id.progress_bar_video_loading)
+                            .visibility = View.GONE
+                    }
+            }
+
+            override fun onVideoDuration(youTubePlayer: YouTubePlayer, duration: Float) {
+
+            }
+
+            override fun onVideoId(youTubePlayer: YouTubePlayer, videoId: String) {
+
+            }
+
+            override fun onVideoLoadedFraction(
+                youTubePlayer: YouTubePlayer,
+                loadedFraction: Float
+            ) {
+
+            }
+
+        })
+
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -67,13 +149,41 @@ class TutorialBottomSheetDialog(val activity: Activity) : BottomSheetDialogFragm
         // Inflate the layout for this fragment
         val v: View = inflater.inflate(R.layout.layout_bottom_sheet_tutorial, container, false)
         val skipButton = v.findViewById<TextView>(R.id.btn_skip)
+        handlePlayerWithInternet(v)
         val youtubeView = v.findViewById<YouTubePlayerView>(R.id.youtube_player_view)
-        youtubeView?.enterFullScreen()
         lifecycle.addObserver(youtubeView)
         skipButton.setOnClickListener {
             dismiss()
         }
         return v
+    }
+
+    private fun handlePlayerWithInternet(v:View){
+        if (AndroidUtils.isOnline(requireActivity())) {
+            //show youtube player
+            showYoutubePlayer(v)
+        }else{
+            //show internet error
+            internetError(v)
+        }
+    }
+
+    private fun internetError(v:View){
+        Toasty.error(requireActivity(),
+            "Please check your internet!").show()
+        val youtubeView = v.findViewById<YouTubePlayerView>(R.id.youtube_player_view)
+        val gifView = v.findViewById<GifImageView>(R.id.gif_no_internet)
+        val retryButton = v.findViewById<MaterialButton>(R.id.button_retry)
+        val loadingAnimation = v.findViewById<LoadingAnimation>(R.id.progress_bar_video_loading)
+        loadingAnimation.visibility = View.GONE
+
+        youtubeView?.visibility = View.GONE
+        gifView?.visibility = View.VISIBLE
+        retryButton?.visibility = View.VISIBLE
+
+        retryButton?.setOnClickListener {
+                handlePlayerWithInternet(v)
+        }
     }
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
