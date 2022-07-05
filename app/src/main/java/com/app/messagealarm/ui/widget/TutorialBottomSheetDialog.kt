19360ca4d0da7ebf5171b174cae6daa9594c46ac
Mujahid Khan <mkhan9047@gmail.com>
2022-07-05 16:45:56 +0600

    added youtube player for tutorial betterment

--- app/src/main/java/com/app/messagealarm/ui/widget/TutorialBottomSheetDialog.kt
+++ app/src/main/java/com/app/messagealarm/ui/widget/TutorialBottomSheetDialog.kt
@@ -2,19 +2,14 @@ package com.app.messagealarm.ui.widget
 
 import android.app.Activity
 import android.app.Dialog
-import android.content.Context
 import android.content.DialogInterface
-import android.media.AudioManager
-import android.net.Uri
 import android.os.Bundle
 import android.util.DisplayMetrics
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.view.WindowManager
 import android.widget.FrameLayout
 import android.widget.TextView
-import android.widget.VideoView
 import com.app.messagealarm.R
 import com.app.messagealarm.ui.main.alarm_applications.AlarmApplicationActivity
 import com.app.messagealarm.utils.Constants
@@ -22,7 +17,13 @@ import com.app.messagealarm.utils.SharedPrefUtils
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.PlayerConstants
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.YouTubePlayer
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.YouTubePlayerListener
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.options.IFramePlayerOptions
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.views.YouTubePlayerView
 import kotlinx.android.synthetic.main.layout_dialog_onboarding.*
+
 
 class TutorialBottomSheetDialog(val activity: Activity) : BottomSheetDialogFragment() {
 
@@ -65,29 +66,13 @@ class TutorialBottomSheetDialog(val activity: Activity) : BottomSheetDialogFragm
     ): View {
         // Inflate the layout for this fragment
         val v: View = inflater.inflate(R.layout.layout_bottom_sheet_tutorial, container, false)
-        val videoView = v.findViewById<VideoView>(R.id.video_view_tutorial)
         val skipButton = v.findViewById<TextView>(R.id.btn_skip)
+        val youtubeView = v.findViewById<YouTubePlayerView>(R.id.youtube_player_view)
+        youtubeView?.enterFullScreen()
+        lifecycle.addObserver(youtubeView)
         skipButton.setOnClickListener {
-            (activity as AlarmApplicationActivity).changeStateOfSpeedDial()
-            //SharedPrefUtils.write(Constants.PreferenceKeys.IS_VIDEO_SHOWED, true)
             dismiss()
         }
-        val path = "android.resource://" + requireActivity().packageName.toString() + "/" + R.raw.video_tutorial
-        videoView.setVideoURI(Uri.parse(path))
-        videoView.setZOrderOnTop(true)
-        videoView.animate().alpha(1f)
-        videoView.seekTo(100)
-        videoView.setOnPreparedListener {
-            requireActivity().window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
-            it.setScreenOnWhilePlaying(true)
-            videoView.setZOrderOnTop(false)
-        }
-        videoView.setOnCompletionListener {
-            skipButton.text = "CLOSE"
-            videoView.stopPlayback()
-            requireActivity().window.clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
-        }
-        videoView.start()
         return v
     }
 
